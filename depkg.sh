d9d0ef3f5a83d7cb30a40ca3149e94a3c4f9e56e
NerdOfLinux <github@nerdoflinux.com>
2020-05-05 12:35:20 -0500

    Rewrite of depkg.sh
    Added support for command-line arguments, and more verbose output.

--- depkg.sh
+++ depkg.sh
@@ -1,39 +1,101 @@
 #!/bin/bash
-echo "Welcome to the depkg shell script. This script will get the .app file out of most .pkg files without the need for admin!"
-#If $1 is empty, then ask for the filename of the .pkg file
-if [ -z "$1" ]
+
+# This script will install a .pkg file on Mac OS without requiring admin privileges
+
+# Instructions
+usage() {
+    cat <<EOF
+Usage for $0
+$0 [-o output] path
+EOF
+    exit 0;
+}
+
+input_file=""
+
+while getopts ":o:" o
+do
+    case "${o}" in
+	o)
+	    output_file="$OPTARG"
+	    ;;
+	*)
+	    usage
+	    ;;
+    esac
+done
+
+# Get input file
+shift $(($OPTIND - 1))
+
+# If the input file is empty
+if [ -z $1 ]
 then
-	echo "What .pkg file would you like to depkg?"
-	read pkgname
-else
-	pkgname=$1
-fi
-#If $2 is empty, then ask for out file
-if [ -z "$2" ]
-then
-    echo "What would you like to name the app?"
-    read out
-else
-    out=$2
+    usage
 fi
 
-#Decompress .pkg file
-pkgname="${pkgname//\\}"
-pkgutil --expand "${pkgname}" temp.tmp
-#Go into folder
-cd temp.tmp
-#File the app file
-payload=$(find . -name "Payload" -size +1000000c)
-#If payload is a file
-if [ -f "${payload}" ]
+input_file="$1"
+
+# Verify the input file exists
+if [ ! -f "$input_file" ]
 then
-    #Decompress app
-    tar -xzf "${payload}" -C ~/Applications
-elif [ -d "${payload}" ]
-then
-    mkdir ~/Applications/"$out"
-    cp -R "${payload}" ~/Applications/"$out".app
+    echo "$input_file doesn not exist"
+    exit 1
 fi
-cd ..
-rm -rf temp.tmp
-echo "$out.app is now in your applications folder"
+
+# Set default output file
+if [ -z $output_file ]
+then
+    output_file=$(basename "$input_file" ".pkg")
+    output_file="$HOME/Applications/${output_file}.app"
+fi
+
+# Make sure the output file doesn't exist
+if [ -e "$output_file" ]
+then
+    echo "$output_file exists"
+    exit 1
+fi
+
+# Extract the pkg file
+expand_dir=$(mktemp -d)
+rmdir "$expand_dir"
+echo "Extracting $input_file to $expand_dir"
+pkgutil --expand "$input_file" "$expand_dir"
+
+# Check the payload
+if [ -f "$expand_dir/Payload" ]
+then
+
+fi
+possible_pkgs=($expand_dir/*.pkg)
+payload_pkg="${possible_pkgs[0]}"
+echo "Found $payload_pkg"
+
+payload="$payload_pkg/Payload"
+
+# Create the output file
+mkdir "$output_file"
+
+# Check for a payload
+if [ -f "$payload" ]
+then
+    tmp=$(mktemp -d)
+    echo "Extracting $payload to $tmp"
+    tar xf "$payload" -C "$tmp"
+    # Find the .app folder
+    possible_apps=($tmp/*.app)
+    app_file="${possible_apps[0]}"
+    echo "Found $app_file"
+    echo "Copying $app_file to $output_file"
+    mv $app_file/* $output_file
+    # Cleanup
+    rm -rf $tmp
+elif [ -d "$payload" ]
+then
+    echo "Renaming payload"
+    mv "$payload" "$output_file"
+else    
+    echo "ERROR! No payload found."
+    exit 1
+fi
